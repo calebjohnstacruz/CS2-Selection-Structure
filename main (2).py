25e4a724da11220e4c25b2453567c20df2294aa7
calebjohnstacruz <calebjohn.stacruz@smc.pshs.edu.ph>
2026-09-07 15:29:55 +0800

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,39 @@
+#Even or Odd
+number = float(input("Please enter a number: "))
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+#Password Gatekeeper
+print("")
+
+word = input("Please enter password: ")
+
+if len(word) >=8:
+  print("Password if sufficient")
+else:
+  print("Too short, I need 8 or more characters!!")
+
+#Movie ticket price
+print("") 
+
+age = int(input("Please enter your age: "))
+
+if age <= 12:
+  print("Ticker price is P180")
+elif age >= 13 and age <=64:
+  print("Ticket price is P250")
+else:
+  print("Ticket price is P200")
+
+#Leap Year Checker
+print("") 
+
+year = int(input("Please enter a year "))
+
+if year % 400 and year % 4 == 0:
+  print(f"{year} is a leap year.")
+else:
+  print(f"{year} is not a leap year.")
